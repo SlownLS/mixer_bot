5776be60bd9eb96603dc185686573a561057af21
SlownLS <slownls.luca@gmail.com>
2020-04-29 00:00:23 +0200

    Reload build

--- build/commands/dons.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = (socket, msg, args) => {
-    socket.call('msg', [`Faites un dons ici: nique ta mère`]);
-};
--- build/commands/ping - Copie.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = (socket, msg, args) => {
-    socket.call('msg', [`Pong lol`]);
-};
